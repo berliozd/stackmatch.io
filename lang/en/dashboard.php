fb6b5086d2d7c3c89eefd32862c5d0eebc947197
Didier Berlioz <didier.berlioz@hagergroup-external.com>
2024-04-25 18:25:17 +0200

    Initial commit

---
+++ lang/en/dashboard.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'not_subscribed' => 'You are not subscribed.',
+    'subscribed' => 'You are subscribed.',
+    'subscription_end_on' => 'You subscription will end on :date',
+    'consider_subscribing_for_full_features' => 'Consider subscribing for full features.'
+];
