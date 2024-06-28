2315baff3358555a8ea53709e3ce3422477b6b52
Didier Berlioz <berliozd@gmail.com>
2024-06-29 01:00:54 +0200

    Add validation on histories

---
+++ resources/js/Components/ErrorAlert.vue
@@ -0,0 +1,30 @@
+<script setup>
+import {onMounted, ref} from "vue";
+
+const props = defineProps({
+  hideAfterDelay: Boolean,
+  error: String
+});
+onMounted(() => {
+  if (!props.hideAfterDelay) {
+    return;
+  }
+  setTimeout(
+      () => hidden.value = 'hidden',
+      5000
+  );
+})
+
+const hidden = ref('opacity-100');
+
+</script>
+
+<template>
+  <div role="alert" class="alert alert-error transition duration-700 ease-out" :class="hidden">
+    <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24">
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+            d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"/>
+    </svg>
+    <span>{{ error }}</span>
+  </div>
+</template>
--- resources/js/Pages/App/Website.vue
+++ resources/js/Pages/App/Website.vue
@@ -2,12 +2,13 @@
 import PageHeader from "@/Components/PageHeader.vue";
 import Box from "@/Components/Box.vue";
 import AppLayout from "@/Layouts/AppLayout.vue";
-
-import {Head, router} from '@inertiajs/vue3';
-import {reactive, ref} from "vue";
 import PrimaryButton from "@/Components/PrimaryButton.vue";
 import TextArea from "@/Components/TextArea.vue";
+
+import {Head, router, usePage} from '@inertiajs/vue3';
+import {reactive, ref} from "vue";
 import {useStore} from "@/Composables/store.js";
+import ErrorAlert from "@/Components/ErrorAlert.vue";
 
 const props = defineProps({id: null})
 const id = window.location.href.split('/').pop();
@@ -16,7 +17,6 @@ const userWebsite = ref(null)
 const form = reactive({
     historyContent: '',
 })
-
 
 if (!id) {
     router.visit(route('websites'));
@@ -34,6 +34,9 @@ const getUserWebsite = async () => {
 getUserWebsite()
 
 const submit = () => {
+    if (!validate(form)) {
+        return;
+    }
     axios.post('/api/histories/add', {'content': form.historyContent, 'user_website_id': id})
         .then(response => {
             useStore().setToast('Added!');
@@ -41,8 +44,18 @@ const submit = () => {
         })
         .catch(error => {
             console.error(error);
+            usePage().props.error = error
         });
 }
+const validate = (form) => {
+    usePage().props.error = ''
+    if (form.historyContent === '') {
+        usePage().props.error = 'History content cannot be empty!'
+        return false;
+    }
+    return true;
+}
+
 </script>
 
 <template>
@@ -64,20 +77,31 @@ const submit = () => {
         <Box>
             <div v-if="userWebsite?.histories.length>0" class="underline mb-2">History</div>
             <div v-else>No history</div>
-            <div v-for="history in userWebsite?.histories" class="mb-2">
-                <div class="bg-neutral-content/40 border rounded p-2 mb-1">
+            <div v-for="history in userWebsite?.histories" class="mb-2 bg-gradient-to-b from-neutral p-1">
+                <div class="">
                     {{ history?.content }}
                 </div>
-                <div class="text-xs text-neutral flex justify-end">Created at : {{ history?.created_at }}</div>
+                <div class="flex md:flex-row flex-col md:space-x-8 justify-end">
+                    <div class="flex md:space-x-4 space-x-2 justify-end">
+                        <div class="text-xs">Edit</div>
+                        <div class="text-xs">Delete</div>
+                    </div>
+                    <div class="text-xs justify-end flex">Created at : {{ history?.created_at }}</div>
+                </div>
             </div>
         </Box>
         <Box>
             <div>Add new history</div>
-            <form @submit.prevent="submit">
-                <div class="mb-2">
-                    <TextArea v-model="form.historyContent"/>
+            <form @submit.prevent="submit" class="">
+                <div class="mb-1">
+                    <TextArea v-model="form.historyContent" class="w-full" :rows="'2'"/>
                 </div>
-                <PrimaryButton type="submit">Add</PrimaryButton>
+                <ErrorAlert v-if="usePage().props.error" :error="usePage().props.error" class="my-2"
+                            :hide-after-delay="true"/>
+                <div class="flex justify-end">
+                    <PrimaryButton type="submit" class="">Add</PrimaryButton>
+                </div>
+                <div v-if="form.errors?.email">{{ form.errors?.email }}</div>
             </form>
         </Box>
     </AppLayout>
