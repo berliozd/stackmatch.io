0048afdaff5d5b36269d85fd1b7b8042eb8d7826
Didier Berlioz <berliozd@gmail.com>
2024-08-14 23:46:11 +0200

    Fix subscription checking

--- resources/js/Composables/App/hasUsedFreeSearches.js
+++ resources/js/Composables/App/hasUsedFreeSearches.js
@@ -2,7 +2,6 @@ import axios from "axios";
 import {usePage} from "@inertiajs/vue3";
 
 export default async function hasUsedFreeSearches() {
-    // return true
     if (usePage().props.subscription && usePage().props.subscription.is_valid) {
         return false
     }
